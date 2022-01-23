d1f3f8efd843fa2e9b60f5ab8ae612fd36cca340
Sulabh Tiwary <sulabhtiwary298@gmail.com>
2022-01-23 18:41:29 +0530

    secrets from os

--- src/config/env.py
+++ src/config/env.py
@@ -1,25 +1,22 @@
 import os
 
-# APP_ENV = os.getenv("APP_ENV")
-# APP_DEBUG = os.getenv("APP_DEBUG") in ["true", "True"]
-# LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
-# DATABASE_PORT = os.getenv("DATABASE_PORT")
-# DATABASE_USER = os.getenv("DATABASE_USER")
-# DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-# DATABASE_HOST=os.getenv("DATABASE_HOST")
-# DATABASE_NAME=os.getenv("DATABASE_NAME")
-# SENTRY_DSN = os.getenv("SENTRY_DSN")
-# ALLOWED_CORS_ORIGINS = os.getenv("ALLOWED_CORS_ORIGINS")
-# SENTRY_DSN = os.getenv("SENTRY_DSN")
-# LOGS_DIR = os.getenv("LOGS_DIR")
+APP_ENV = os.getenv("APP_ENV")
+APP_DEBUG = os.getenv("APP_DEBUG") in ["true", "True"]
+LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST=os.getenv("DATABASE_HOST")
+DATABASE_NAME=os.getenv("DATABASE_NAME")
+SENTRY_DSN = os.getenv("SENTRY_DSN")
+ALLOWED_CORS_ORIGINS = os.getenv("ALLOWED_CORS_ORIGINS")
+SENTRY_DSN = os.getenv("SENTRY_DSN")
+LOGS_DIR = os.getenv("LOGS_DIR")
+JWT_SECRET = os.getenv("JWT_SECRET")
+ACCOUNT_SID = os.getenv("ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+SID = os.getenv("SID")
+CLOUDINARY_CLOUD_NAME=os.getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
 
-APP_ENV = "local"
-APP_DEBUG = True
-LOG_LEVEL = "DEBUG"
-DATABASE_PORT = 3356
-DATABASE_USER = "root"
-DATABASE_PASSWORD = "root"
-DATABASE_HOST = "127.0.0.1"
-DATABASE_NAME = "khunti_baazar"
-LOGS_DIR = "/usr/src/backend/logs"
-JWT_SECRET = "test_secret"
